938c13ad2d2847b82cc00b9ce2731c640e1f769c
Dorin Cucicov <cucicov@gmail.com>
2023-03-19 16:00:51 +0200

    interval posting

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import discord
 import os
+from discord.ext import tasks
+import time
 
 from dotenv import load_dotenv
 
 client = discord.Client(intents=discord.Intents.default())
 
-TOKEN='MTA4Njk3NTQ4MjMyMDM5NjI5OQ.GVVczL.g7RNMT1akcx6TXKwiNGWHFGKGOX0ZlYlf-fa9c'
+TOKEN='XX'
 
 @client.event
 async def on_message(message):
@@ -18,12 +20,18 @@ async def on_message(message):
     if message.content.startswith('$private'):
         await message.channel.send("Hello00000")
 
-
-
 @client.event
 async def on_ready():
     print("Bot connected to the server!")
     channel = client.get_guild(998234966284582942).get_channel(998234966284582945)
     await channel.send("hey, guys!")
+    await myLoop.start()
+
+@tasks.loop(seconds = 10) # repeat after every 10 seconds
+async def myLoop():
+    await client.wait_until_ready()
+    channel = client.get_guild(998234966284582942).get_channel(998234966284582945)
+    await channel.send(time.time())
+
 
 client.run(TOKEN)
