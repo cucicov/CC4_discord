743842f52dc3835658b229d8be9185604bc6ee51
Dorin Cucicov <cucicov@gmail.com>
2023-04-02 13:17:10 +0300

    dynamic ids

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import discord
 import os
 from discord.ext import tasks
 import time
+import logging
 
-from dotenv import load_dotenv
+logging.basicConfig(filename='bot.log', level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
 
 # intents = discord.Intents.all()
 intents = discord.Intents.default()
@@ -14,7 +15,7 @@ TOKEN=''
 
 @client.event
 async def on_message(message):
-    print('message', message.content)
+    logging.info(f'{message.author}: {message.content}')
     if message.author == client.user:
         return
 
@@ -24,15 +25,32 @@ async def on_message(message):
     if message.content.startswith('$private'):
         await message.channel.send("Hello00000")
 
+# @client.event
+# async def on_member_join(member):
+#     logging.info(f'{member} joined the server')
+#
+# @client.event
+# async def on_member_remove(member):
+#     logging.info(f'{member} left the server')
+
 @client.event
-async def on_ready():
-    print("Bot connected to the server!")
-    channel = client.get_guild(998234966284582942).get_channel(998234966284582945)
+async def on_guild_join(guild):
+    logging.info(f'Joined server: {guild.id}')
+
+    channels = guild.text_channels
+    voice_channels = guild.voice_channels
+
+    channel = client.get_guild(guild.id).get_channel(channels[0].id)
     await channel.send("hey, guys!")
 
-    channelVoice = client.get_guild(998234966284582942).get_channel(998234966284582946)
-    voice = await channelVoice.connect()
+    channel_voice = client.get_guild(guild.id).get_channel(voice_channels[0].id)
+    voice = await channel_voice.connect()
     voice.play(discord.FFmpegPCMAudio("C:/Users/dorin/Downloads/sample.mp3"))
+
+@client.event
+async def on_ready():
+    logging.info(f'Logged in as {client.user}')
+
     # while voice.is_playing():
     #     time.sleep(.1)
     # await voice.disconnect()
